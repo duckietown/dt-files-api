fe3661db202048c684026e95ad8df25d560b00a6
Andrea F. Daniele <afdaniele@ttic.edu>
2019-12-05 01:07:37 -0600

    added compression

--- code/files_api/api.py
+++ code/files_api/api.py
@@ -6,7 +6,13 @@ from http.server import \
   BaseHTTPRequestHandler, \
   SimpleHTTPRequestHandler
 
+from .archive import Zip, Tar
+
 BUFFER_SIZE_BYTES = 4 * 1024 * 1024  # 4MB
+FORMAT_TO_ARCHIVE = {
+  'zip': Zip,
+  'tar': Tar,
+}
 
 
 class ListingsServer(DTProcess, ThreadingHTTPServer):
@@ -55,9 +61,9 @@ class FilesAPIHTTPRequestHandler(BaseHTTPRequestHandler):
     def __init__(self, request, client_address, server):
         BaseHTTPRequestHandler.__init__(self, request, client_address, server)
 
-    def _set_headers(self, code, content_length, mime_type='text/plain', mime_enc=None):
+    def set_headers(self, code, message, content_length, mime_type='text/plain', mime_enc=None):
         # open headers
-        self.send_response(code)
+        self.send_response(code, message)
         # set content length
         self.send_header('Content-Length', content_length)
         # set MIME type and encoding
@@ -71,49 +77,65 @@ class FilesAPIHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
+        parts = (self.path + '?').split('?')
+        filepath_req, args_str = parts[0], parts[1]
+        args = parse_args(args_str)
+        # check arguments
+        if 'format' in args and args['format'] not in FORMAT_TO_ARCHIVE:
+            self.send_error(400, 'Bad Request', f'Format {args["format"]} not supported.')
         # get requested file from request object
-        filepath = os.path.join(self.server.data_dir, self.path)
-        self.server.logger.debug(f'Requesting: GET:[{filepath}]')
+        filepath = os.path.join(self.server.data_dir, filepath_req[1:])
+        self.server.logger.debug(f'Requesting: GET:[{filepath_req}]')
+        # check if the path exists
+        if not os.path.exists(filepath):
+            self.send_error(404, 'Not Found', f'Resource {filepath_req} not found.')
+        # we can't deliver directories without an archive format
+        if 'format' not in args and os.path.isdir(filepath):
+            self.send_error(400, 'Bad Request', f'Path {filepath_req} is a directory. A format is required.')
         # deliver files on match
-        if os.path.exists(filepath) and os.path.isfile(filepath):
+        if 'format' not in args and os.path.isfile(filepath):
             # figure out the MIME type/enc for the requested file
             mime_type, mime_enc = mimetypes.guess_type(filepath)
             # get file size
             content_length = os.path.getsize(filepath)
             # send headers
-            self._set_headers(200, content_length, mime_type, mime_enc)
+            self.set_headers(200, 'OK', content_length, mime_type, mime_enc)
             # send file
             with open(filepath, 'rb') as fin:
-              self.wfile.write(fin.read(BUFFER_SIZE_BYTES))
+              transfer_bytes(fin, self.wfile)
             return
-        # the filepath is not an existing file
-        # if os.path.exists(filepath) and os.path.isdir(filepath):
-        #
-
-
-
-
-
-
-
-        # code_loader_status = self.server.code_loader.get_status()
-        # if not INCLUDE_OUTPUT:
-        #     for lvl in code_loader_status['progress']:
-        #         code_loader_status['progress'][lvl]['output'] = None
-        # res = json.dumps(code_loader_status, indent=4, sort_keys=True).encode()
-
-
-        # self.wfile.write('Got: '+file)
+        # compress the resource
+        archive = FORMAT_TO_ARCHIVE[args['format']]()
+        archive.add(filepath, filepath_req)
+        mime_type, mime_enc = archive.mime()
+        # return archive
+        self.set_headers(200, 'OK', archive.size(), mime_type, mime_enc)
+        # send file
+        transfer_bytes(archive.data(), self.wfile)
 
     def do_POST(self):
         # get requested file from request object
         file = self.path
         self.server.logger.debug(f'Requesting: POST:[{file}] w/ args {None}')
-        self._set_headers()
+        self.set_headers()
 
     def do_HEAD(self):
         self.server.logger.debug(f'Requesting: HEAD')
-        self._set_headers()
+        self.set_headers()
 
     def log_message(self, format, *args):
         return
+
+
+def parse_args(args_str):
+    return {
+        e.split('=')[0]: (e+'=').split('=')[1]
+        for e in args_str.split('&')
+    }
+
+def transfer_bytes(bytes_in, socket_out):
+    while True:
+        chunk = bytes_in.read(BUFFER_SIZE_BYTES)
+        if not chunk:
+            break
+        socket_out.write(chunk)
--- code/files_api/archive.py
+++ code/files_api/archive.py
@@ -1,28 +1,65 @@
+import os
 import io
 import zipfile
-
-BUFFER = {
-  'zip': lambda
-}
+import tarfile
+from abc import ABC, abstractmethod
 
 
-class Archive(object):
+class Archive(ABC):
 
   def __init__(self):
     self._buffer = io.BytesIO()
 
-  def add_file(self, name, content):
+  @abstractmethod
+  def add(self, file, arcname):
     pass
+
+  @abstractmethod
+  def mime(self):
+    pass
+
+  def data(self):
+    self._buffer.seek(0)
+    return self._buffer
+
+  def size(self):
+    return self._buffer.getbuffer().nbytes
 
 
 class Zip(Archive):
 
-  def add_file(self, name, content):
-    with zipfile.ZipFile(self._buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
-      zip_file.writestr(name, content)
+  def add(self, filepath, arcname):
+    if not os.path.exists(filepath):
+      raise ValueError(f'File {filepath} not found.')
+    for file, aname in listfiles(filepath, arcname):
+      mode = "a" if self.size() else "w"
+      with zipfile.ZipFile(self._buffer, mode, zipfile.ZIP_DEFLATED, False) as zip_file:
+        zip_file.write(file, aname)
+
+  def mime(self):
+    return 'application/zip', None
+
 
 
 class Tar(Archive):
 
-  def add_file(self, name, content):
-    pass
+  def add(self, filepath, arcname):
+    if not os.path.exists(filepath):
+      raise ValueError(f'File {filepath} not found.')
+    mode = "a" if self.size() else "w"
+    with tarfile.TarFile(fileobj=self._buffer, mode=mode) as tar_file:
+      tar_file.add(filepath, arcname)
+
+  def mime(self):
+    return 'application/x-gzip', None
+
+
+def listfiles(filepath, arcpath):
+  if os.path.isfile(filepath):
+    yield filepath, arcpath
+  return
+  # ---
+  for root, _, files in os.walk(filepath):
+    for filename in files:
+      arcname = os.path.join(arcpath, root[len(filepath):], filename)
+      yield os.path.join(root, filename), arcname
